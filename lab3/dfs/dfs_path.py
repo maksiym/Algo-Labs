e84fb0af7ca2380a805fc78f2d6cb55ac378de43
maks morochenets <maksmorroo@gmail.com>
2022-10-07 21:03:14 +0300

    added final solution funcs (◑_◑)

--- lab3/dfs/dfs_path.py
+++ lab3/dfs/dfs_path.py
@@ -1,3 +1,39 @@
+class XPNode:
+    def __init__(self, xp, l_root=None, r_root=None):
+        self.xp = xp
+        self.l_root = l_root
+        self.r_root = r_root
 
-def find_path_by_dfs(graph: dict) -> list:
-    pass
+    def __str__(self):
+        return f'{self.xp}'
+
+
+class DFS:
+    def find_path_sum_by_dfs(self, root: XPNode) -> int:
+        res = [root.xp]
+        if not root:
+            return 0
+
+        def dfs(root):
+            left_max = 0
+            right_max = 0
+            if root.l_root is not None:
+                left_max = dfs(root.l_root)
+            if root.r_root is not None:
+                right_max = dfs(root.r_root)
+
+            res[0] = max(res[0], root.xp + left_max, root.xp + right_max)
+
+            return root.xp + max(left_max, right_max)
+
+        dfs(root)
+        return res[0]
+
+    def do_xp_nodes(self, xp_nodes):
+        res = []
+        for xp in xp_nodes:
+            for x in xp:
+                result = self.find_path_sum_by_dfs(x)
+                res.append(result)
+
+        return max(res)
--- lab3/main.py
+++ lab3/main.py
@@ -1,22 +1,40 @@
-from lab3.dfs.dfs_path import find_path_by_dfs
+from lab3.dfs.dfs_path import *
 
 
-def create_from_input_file(input_file_name: str):
-    values = []
-    xp_values = []
+def create_from_input_file(input_file_name: str) -> DFS:
+    lines = []
     try:
         with open(input_file_name) as input_file:
-            lines = input_file.readlines()
-            for line in lines:
-                values += line.rstrip().split(' ')
+            lines = input_file.read().splitlines()
     except FileNotFoundError:
         print('._.')
 
-    values = list(map(int, xp_values))
+    height = int(lines[0])
+    temp = []
 
-    graph_ladder = {}
+    for i in range(1, len(lines)):
+        temp.append(list(map(int, lines[i].split(' '))))
 
-    return graph_ladder
+    for t in temp:
+        for i in range(0, len(t)):
+            t[i] = XPNode(t[i])
+    print()
+
+    for i in range(0, len(temp)):
+        for j in range(0, len(temp[i])):
+            if temp[i][j] == temp[0][0]:
+                break
+            elif temp[i][j] == temp[i][0]:
+                temp[i][j].r_root = temp[i - 1][j]
+            elif temp[i][j] == temp[i][-1]:
+                temp[i][j].l_root = temp[i - 1][j - 1]
+            else:
+                temp[i][j].l_root = temp[i - 1][j - 1]
+                temp[i][j].r_root = temp[i - 1][j]
+
+    print()
+
+    return temp
 
 
 def write_into_output_file(output_file_name: str, result_xp: int) -> None:
@@ -28,11 +46,15 @@ def write_into_output_file(output_file_name: str, result_xp: int) -> None:
 
 
 def main():
-    graph_ladder = create_from_input_file('input.txt')
+    temp = create_from_input_file('resources/career.in')
+    temp1 = create_from_input_file('resources/career1.in')
 
-    result_xp_path = find_path_by_dfs(graph_ladder)
+    graph_ladder = DFS()
+    result_xp_path = graph_ladder.do_xp_nodes(temp)
+    result_xp_path1 = graph_ladder.do_xp_nodes(temp1)
 
-    write_into_output_file('output.txt', len(result_xp_path))
+    write_into_output_file('career.out', result_xp_path)
+    write_into_output_file('career1.out', result_xp_path1)
 
 
 if __name__ == '__main__':
